5f946bc2e2d87f45ccbb5402c71ff1fd9ffe8369
santimarro <smarro@gmail.com>
2016-09-29 15:44:16 -0300

    Cambiado nombre de paquete de verdad

--- app/src/androidTest/java/ar/edu/unc/famaf/redditreader/ApplicationTest.java
+++ app/src/androidTest/java/ar/edu/unc/famaf/redditreader/ApplicationTest.java
@@ -1,4 +1,4 @@
-package famaf.unc.edu.ar.redditreader;
+package ar.edu.unc.famaf.redditreader;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/ar/edu/unc/famaf/redditreader/ExampleUnitTest.java
+++ app/src/test/java/ar/edu/unc/famaf/redditreader/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package famaf.unc.edu.ar.redditreader;
+package ar.edu.unc.famaf.redditreader;
 
 import org.junit.Test;
 
