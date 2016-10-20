23dcdf631c15f45796d25527f53d6176bbd3d5c9
Santiago Marro <smarro@gmail.com>
2016-10-20 19:42:03 -0300

    Comienzo de pegarle a la api

--- app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
@@ -1,13 +1,27 @@
 package ar.edu.unc.famaf.redditreader.backend;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import ar.edu.unc.famaf.redditreader.PostAdapter;
 import ar.edu.unc.famaf.redditreader.R;
 import ar.edu.unc.famaf.redditreader.model.PostModel;
 
 public class Backend {
     private static Backend ourInstance = new Backend();
+    private final String URL_TEMPLATE = "http://www.reddit.com/r/" +".json" + "?after=AFTER";
 
     public static Backend getInstance() {
         return ourInstance;
@@ -16,49 +30,39 @@ public class Backend {
     private List<PostModel> mListPostModel;
     private Backend() {
         mListPostModel = new ArrayList<>();
+    }
 
-        PostModel p1 = new PostModel("This is not concrete..this is New Delhi","ophionn","r/todayilearned", 1000, "4 hs", "https://b.thumbs.redditmedia.com/xj1jR4ke30GHaSNtP-zs8HL7eWh46hTTBzdohs2CnkU.jpg");
-        mListPostModel.add(p1);
+    private class GetTopPostsTask extends AsyncTask<URL, Integer, List<PostModel>> {
 
-        PostModel p2 = new PostModel("It is true","kenny","r/todayilearned",56, "1 hs", "https://b.thumbs.redditmedia.com/Tp9CqdhxKiMiNXmhqc_QFTWGw1GipKcCc9_PmZ3z5-M.jpg");
-        mListPostModel.add(p2);
+        public GetTopPostsTask() {
+        
+        }
+        @Override
+        protected List<PostModel> doInBackground(URL... urls) {
+            List<PostModel> list = new ArrayList<>();
+            URL url = urls[0];
+            HttpURLConnection connection;
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+                InputStream is = connection.getInputStream();
+                String data = is.toString();
+                list = parser(data);
 
-        PostModel p3 = new PostModel("My mom cut into her carrot and found a smiley face.","mark","r/radiohead", 23, "1 hs", "https://b.thumbs.redditmedia.com/za3XzCLzP7oB8a53lGJM2ziznNe5NA3IidRoKnz2Wws.jpg");
-        mListPostModel.add(p3);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return list;
+        }
 
-        PostModel p4 = new PostModel("First smile ever","john","r/funny", 23, "1 hs", "https://a.thumbs.redditmedia.com/KP-qNPsmpmJYRkRUUB3ZzvpHJFtiQum3EHav9-6TWz8.jpg");
-        mListPostModel.add(p4);
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            System.out.println("onPostExecute");
 
-        PostModel p5 = new PostModel("The neighbors dog loves me","turk","r/aww", 23, "1 hs", "https://b.thumbs.redditmedia.com/gc8XIvLJaqBnuI4VvQZ2kir89DpBVri45eAn_HZ_raY.jpg");
-        mListPostModel.add(p5);
-        PostModel p6 = new PostModel("This is not concrete..this is New Delhi","ophionn","r/todayilearned", 1000, "4 hs", "https://b.thumbs.redditmedia.com/xj1jR4ke30GHaSNtP-zs8HL7eWh46hTTBzdohs2CnkU.jpg");
-        mListPostModel.add(p6);
+            if (isCancelled()) {
+                result = null;
+            }
 
-        PostModel p7 = new PostModel("It is true","kenny","r/todayilearned",56, "1 hs", "https://b.thumbs.redditmedia.com/Tp9CqdhxKiMiNXmhqc_QFTWGw1GipKcCc9_PmZ3z5-M.jpg");
-        mListPostModel.add(p7);
-
-        PostModel p8 = new PostModel("My mom cut into her carrot and found a smiley face.","mark","r/radiohead", 23, "1 hs", "https://b.thumbs.redditmedia.com/za3XzCLzP7oB8a53lGJM2ziznNe5NA3IidRoKnz2Wws.jpg");
-        mListPostModel.add(p8);
-
-        PostModel p9 = new PostModel("First smile ever","john","r/funny", 23, "1 hs", "https://a.thumbs.redditmedia.com/KP-qNPsmpmJYRkRUUB3ZzvpHJFtiQum3EHav9-6TWz8.jpg");
-        mListPostModel.add(p9);
-
-        PostModel p10 = new PostModel("The neighbors dog loves me","turk","r/aww", 23, "1 hs", "https://b.thumbs.redditmedia.com/gc8XIvLJaqBnuI4VvQZ2kir89DpBVri45eAn_HZ_raY.jpg");
-        mListPostModel.add(p10);
-        PostModel p11 = new PostModel("This is not concrete..this is New Delhi","ophionn","r/todayilearned", 1000, "4 hs", "https://b.thumbs.redditmedia.com/xj1jR4ke30GHaSNtP-zs8HL7eWh46hTTBzdohs2CnkU.jpg");
-        mListPostModel.add(p11);
-
-        PostModel p12 = new PostModel("It is true","kenny","r/todayilearned",56, "1 hs", "https://b.thumbs.redditmedia.com/Tp9CqdhxKiMiNXmhqc_QFTWGw1GipKcCc9_PmZ3z5-M.jpg");
-        mListPostModel.add(p12);
-
-        PostModel p13 = new PostModel("My mom cut into her carrot and found a smiley face.","mark","r/radiohead", 23, "1 hs", "https://b.thumbs.redditmedia.com/za3XzCLzP7oB8a53lGJM2ziznNe5NA3IidRoKnz2Wws.jpg");
-        mListPostModel.add(p13);
-
-        PostModel p14 = new PostModel("First smile ever","john","r/funny", 23, "1 hs", "https://a.thumbs.redditmedia.com/KP-qNPsmpmJYRkRUUB3ZzvpHJFtiQum3EHav9-6TWz8.jpg");
-        mListPostModel.add(p14);
-
-        PostModel p15 = new PostModel("The neighbors dog loves me","turk","r/aww", 23, "1 hs", "https://b.thumbs.redditmedia.com/gc8XIvLJaqBnuI4VvQZ2kir89DpBVri45eAn_HZ_raY.jpg");
-        mListPostModel.add(p15);
+        }
     }
 
     public List<PostModel> getTopPosts() {
